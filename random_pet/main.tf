c8b8b3a817d6b3b2dd11579aee2780d7f05cb887
dbeleva-af <dbeleva_af@abv.bg>
2023-06-23 09:47:40 +0300

    Update main.tf

--- random_pet/main.tf
+++ random_pet/main.tf
@@ -11,6 +11,33 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+
+
+
+
+
+
+
+
+
+
 resource "random_pet" "server" {
   keepers = {
     ami_id = ata.aws_ami.ubuntu.id
