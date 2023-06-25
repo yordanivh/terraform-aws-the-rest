9e06b82458b10a7fc268b3426ad4c7a24b8a92f1
dbeleva-af <dbeleva_af@abv.bg>
2023-06-25 18:23:19 +0300

    Update variables.tf

--- variables/variables.tf
+++ variables/variables.tf
@@ -9,3 +9,16 @@ variable "instance_type" {
   type        = string
   value       = t2.micro
 }
+
+variable "vpc-cidr" {
+  description = "AWS VPC for the subnets"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet-cidr" {
+  description = "Subnets for aws_vpc"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
